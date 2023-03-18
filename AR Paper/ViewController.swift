135f3b13554d0fea2a65f9baafe74536bf7c6806
Niko Berdzenishvili <nikolozisia@gmail.com>
2023-03-18 17:25:32 +0400

    Initial Commit

---
+++ AR Paper/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AR Paper
+//
+//  Created by Niko on 18.03.23.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
